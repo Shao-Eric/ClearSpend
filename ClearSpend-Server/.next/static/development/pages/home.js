8c02b9635eb31c7560ab022f5c9d332c042fa0d2
Deren <derenlin123456@gmail.com>
2019-02-16 00:36:44 -0500

    nextjs and nodejs set up

---
+++ ClearSpend-Server/.next/static/development/pages/home.js
@@ -0,0 +1,82 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static\\development\\pages\\home.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fhome&absolutePagePath=C%3A%5CUsers%5Cderen%5CDesktop%5CClearSpend%5CClearSpend%5CClearSpend-Server%5Cpages%5Chome.js!./":
+/*!***************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fhome&absolutePagePath=C%3A%5CUsers%5Cderen%5CDesktop%5CClearSpend%5CClearSpend%5CClearSpend-Server%5Cpages%5Chome.js ***!
+  \***************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/home", function() {
+      var page = __webpack_require__(/*! ./pages/home.js */ "./pages/home.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/home.js */ "./pages/home.js", function() {
+          if(!next.router.components["/home"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/home.js */ "./pages/home.js")
+          next.router.update("/home", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_9c75acfe53428ad7a9fa ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_9c75acfe53428ad7a9fa */ "dll-reference dll_9c75acfe53428ad7a9fa"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/home.js":
+/*!***********************!*\
+  !*** ./pages/home.js ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+
+function Home() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "Hello Next.js");
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ }),
+
+/***/ 1:
+/*!*******************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fhome&absolutePagePath=C%3A%5CUsers%5Cderen%5CDesktop%5CClearSpend%5CClearSpend%5CClearSpend-Server%5Cpages%5Chome.js ***!
+  \*******************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fhome&absolutePagePath=C%3A%5CUsers%5Cderen%5CDesktop%5CClearSpend%5CClearSpend%5CClearSpend-Server%5Cpages%5Chome.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fhome&absolutePagePath=C%3A%5CUsers%5Cderen%5CDesktop%5CClearSpend%5CClearSpend%5CClearSpend-Server%5Cpages%5Chome.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_9c75acfe53428ad7a9fa":
+/*!*******************************************!*\
+  !*** external "dll_9c75acfe53428ad7a9fa" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_9c75acfe53428ad7a9fa;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]));;
+//# sourceMappingURL=home.js.map
